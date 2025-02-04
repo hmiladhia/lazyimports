24aa2d9b5ea3f752d76fd3982ed18091e5c520f0
Dhia Hmila <dhiahmila@gmail.com>
2025-02-04 17:01:27 +0100

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Lazyimports
+![PyPI](https://img.shields.io/pypi/v/lazyimports)
+![PyPI - License](https://img.shields.io/pypi/l/lazyimports)
+![PyPI - Wheel](https://img.shields.io/pypi/wheel/lazyimports)
+![Tests](https://github.com/hmiladhia/lazyimports/actions/workflows/tests.yml/badge.svg)
+[![Copier](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/copier-org/copier/master/img/badge/badge-grayscale-inverted-border-orange.json)](https://github.com/copier-org/copier)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+
+A module to enable lazy imports using native python syntax
+
+## Quick Start
+
+```
+pip install lazyimports
+```
+
+## Contribute
+...
---
+++ src/lazyimports.py
@@ -0,0 +1,151 @@
+from __future__ import annotations
+
+import sys
+import contextlib
+from types import ModuleType
+from typing import TYPE_CHECKING
+from importlib.abc import Loader, MetaPathFinder
+from importlib.machinery import PathFinder, ModuleSpec
+
+if TYPE_CHECKING:
+    from typing import Any
+    from collections.abc import Iterable, Sequence, Generator
+
+__author__ = "Dhia Hmila"
+__version__ = "0.0.1"
+
+lazy_modules: list[str] = []
+
+
+def _load_parent_module(fullname: str) -> None:
+    if not (parent := ".".join(fullname.split(".")[:-1])):
+        return
+
+    if not (parent_module := sys.modules.get(parent)):
+        return
+
+    if isinstance(parent_module, LazyModule):
+        _load_module(parent_module)
+
+
+def _load_module(module: ModuleType) -> None:
+    _load_parent_module(module.__name__)
+
+    if (spec := module.__spec__) is None:
+        return
+
+    if (loader := spec.loader) is None:
+        return
+
+    if not hasattr(loader, "exec_module"):
+        loader.load_module(module.__name__)
+    else:
+        loader.exec_module(module)
+
+
+class LazyModule(ModuleType):
+    def __getattribute__(self, item: str) -> Any:  # noqa ANN401
+        if item in ("__doc__",):
+            raise AttributeError(item)  # trigger loading
+
+        return super().__getattribute__(item)
+
+    def __getattr__(self, item: str) -> Any:  # noqa ANN401
+        if item in ("__path__", "__file__", "__cached__"):
+            raise AttributeError(item)
+
+        _load_module(self)
+
+        return getattr(self, item)
+
+    def __dir__(self) -> Iterable[str]:
+        _load_module(self)
+        return dir(self)
+
+    def __setattr__(self, attr: str, value: Any) -> None:  # noqa ANN401
+        if attr in (
+            "__path__",
+            "__file__",
+            "__cached__",
+            "__loader__",
+            "__package__",
+            "__spec__",
+            "__class__",
+        ):
+            return super().__setattr__(attr, value)
+
+
+        if isinstance(value, ModuleType):
+            return super().__setattr__(attr, value)
+
+        set_attribute = super().__setattr__
+        _load_module(self)
+        return set_attribute(attr, value)
+
+
+class LazyLoaderWrapper(Loader):
+    def __init__(self, loader: Loader) -> None:
+        self.loader = loader
+        self.please_load = False
+
+    def create_module(self, spec: ModuleSpec) -> ModuleType:
+        # mod = self.loader.create_module(spec)
+        return LazyModule(spec.name)
+
+    def exec_module(self, module: ModuleType) -> None:
+        if not self.please_load:
+            self.please_load = True
+            return None
+
+        self._cleanup(module)
+        return self.loader.exec_module(module)
+
+    def _cleanup(self, module: ModuleType) -> None:
+        if module.__spec__ is not None:
+            module.__spec__.loader = self.loader
+        module.__class__ = ModuleType
+
+
+class LazyPathFinder(MetaPathFinder):
+    def __init__(self, module_names: list[str]) -> None:
+        self.lazy_modules = module_names
+        self.finder = PathFinder()
+
+    def find_spec(
+        self,
+        fullname: str,
+        path: Sequence[str] | None = None,
+        target: ModuleType | None = None,
+    ) -> ModuleSpec | None:
+        if fullname not in self.lazy_modules:
+            _load_parent_module(fullname)
+
+            return None
+
+        spec = self.finder.find_spec(fullname, path, target)
+        if spec is None:
+            return None
+
+        if spec.loader is None:
+            return None
+
+        spec.loader = LazyLoaderWrapper(spec.loader)
+        return spec
+
+
+@contextlib.contextmanager
+def lazy_imports(*modules: str) -> Generator[None, None, None]:
+    original_value = lazy_modules[:]
+
+    try:
+        lazy_modules.extend(modules)
+        yield
+    finally:
+        lazy_modules[:] = original_value
+
+
+def install() -> None:
+    sys.meta_path.insert(0, LazyPathFinder(lazy_modules))
+
+
+install()
